ee2a1bc8d67ab39bb6caa9c3aff4ea84a17459d1
Felix S. Klock II <pnkfelix@pnkfx.org>
2019-11-15 15:10:40 +0100

    decommentification

--- tock/chips/arty_e21/src/lib.rs
+++ tock/chips/arty_e21/src/lib.rs
@@ -4,31 +4,6 @@
 #![crate_name = "arty_e21"]
 #![crate_type = "rlib"]
 
-#[cfg(not_now)]
-mod interrupts {
-#![allow(dead_code)]
-
-pub const MTIP: u32 = 7; // Machine Timer
-
-pub const GPIO0: u32 = 18;
-pub const GPIO1: u32 = 19;
-pub const GPIO2: u32 = 20;
-pub const GPIO3: u32 = 21;
-pub const GPIO4: u32 = 22;
-pub const GPIO5: u32 = 23;
-pub const GPIO6: u32 = 24;
-pub const GPIO7: u32 = 25;
-pub const GPIO8: u32 = 26;
-pub const GPIO9: u32 = 27;
-pub const GPIO10: u32 = 28;
-pub const GPIO11: u32 = 29;
-pub const GPIO12: u32 = 30;
-pub const GPIO13: u32 = 31;
-pub const GPIO14: u32 = 32;
-pub const GPIO15: u32 = 33;
-pub const UART0: u32 = 16;
-}
-
 pub mod chip {
 use kernel;
 use rv32i;
@@ -44,19 +19,6 @@ pub struct ArtyExx {
 
 impl ArtyExx {
     pub unsafe fn new() -> ArtyExx { loop { } }
-}
-
-#[cfg(not_now)]
-impl ArtyExx {
-    pub fn enable_all_interrupts(&self) { loop { } }
-
-    pub unsafe fn disable_pmp(&self) { loop { } }
-
-    pub unsafe fn disable_machine_timer(&self) { loop { } }
-
-    pub unsafe fn configure_trap_handler(&self) { loop { } }
-
-    pub unsafe fn initialize(&self) { loop { } }
 }
 
 impl kernel::Chip for ArtyExx {
@@ -87,91 +49,4 @@ impl kernel::Chip for ArtyExx {
 
 #[export_name = "_disable_interrupt_trap_handler"]
     pub extern "C" fn disable_interrupt_trap_handler(_: u32) { loop { } }
-}
-#[cfg(not_now)]
-mod gpio {
-use core::ops::{Index, IndexMut};
-
-use kernel::common::StaticRef;
-use sifive::gpio::{pins, GpioPin, GpioRegisters};
-
-const GPIO0_BASE: StaticRef<GpioRegisters> =
-    unsafe { StaticRef::new(0x2000_2000 as *const GpioRegisters) };
-
-pub struct Port {
-    pins: [GpioPin; 16],
-}
-
-#[cfg(not_now)]
-impl Index<usize> for Port {
-    type Output = GpioPin;
-
-    fn index(&self, index: usize) -> &GpioPin { loop { } }
-}
-
-#[cfg(not_now)]
-impl IndexMut<usize> for Port {
-    fn index_mut(&mut self, index: usize) -> &mut GpioPin { loop { } }
-}
-
-pub static mut PORT: Port = Port {
-    pins: [
-        GpioPin::new(GPIO0_BASE, pins::pin0, pins::pin0::SET, pins::pin0::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin1, pins::pin1::SET, pins::pin1::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin2, pins::pin2::SET, pins::pin2::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin3, pins::pin3::SET, pins::pin3::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin4, pins::pin4::SET, pins::pin4::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin5, pins::pin5::SET, pins::pin5::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin6, pins::pin6::SET, pins::pin6::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin7, pins::pin7::SET, pins::pin7::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin8, pins::pin8::SET, pins::pin8::CLEAR),
-        GpioPin::new(GPIO0_BASE, pins::pin9, pins::pin9::SET, pins::pin9::CLEAR),
-        GpioPin::new(
-            GPIO0_BASE,
-            pins::pin10,
-            pins::pin10::SET,
-            pins::pin10::CLEAR,
-        ),
-        GpioPin::new(
-            GPIO0_BASE,
-            pins::pin11,
-            pins::pin11::SET,
-            pins::pin11::CLEAR,
-        ),
-        GpioPin::new(
-            GPIO0_BASE,
-            pins::pin12,
-            pins::pin12::SET,
-            pins::pin12::CLEAR,
-        ),
-        GpioPin::new(
-            GPIO0_BASE,
-            pins::pin13,
-            pins::pin13::SET,
-            pins::pin13::CLEAR,
-        ),
-        GpioPin::new(
-            GPIO0_BASE,
-            pins::pin14,
-            pins::pin14::SET,
-            pins::pin14::CLEAR,
-        ),
-        GpioPin::new(
-            GPIO0_BASE,
-            pins::pin15,
-            pins::pin15::SET,
-            pins::pin15::CLEAR,
-        ),
-    ],
-};
-}
-#[cfg(not_now)]
-mod uart {
-use kernel::common::StaticRef;
-use sifive::uart::{Uart, UartRegisters};
-
-pub static mut UART0: Uart = Uart::new(UART0_BASE, 32_000_000);
-
-const UART0_BASE: StaticRef<UartRegisters> =
-    unsafe { StaticRef::new(0x2000_0000 as *const UartRegisters) };
 }
